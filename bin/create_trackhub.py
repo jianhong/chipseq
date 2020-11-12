20722dc5a9f8f01f7edc46ef4ffae0760494b9ed
Jianhong Ou <jianhongou@Jianhongs-MacBook-Pro.local>
2020-11-12 11:22:46 -0500

    fix a typo in create_trackhub.

--- bin/create_trackhub.py
+++ bin/create_trackhub.py
@@ -105,7 +105,7 @@ def create_trackhub(OutFolder,ListFile,Genome,EMAIL,PathPrefix=''):
             tracktype=TrackType[extension],
             autoScale='on')
           trackdb.add_tracks(track)
-          linkname=os.path.join(OutFolder, genome, os.path.basename(ifile))
+          linkname=os.path.join(OutFolder, Genome, os.path.basename(ifile))
           os.symlink(ifile, linkname)
         else:
           pass
